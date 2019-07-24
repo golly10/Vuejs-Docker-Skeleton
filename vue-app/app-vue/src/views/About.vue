53c39a06ee67d80d5e22271ed93227158d9c6444
Juanjo Méndez Torrero <juanjo96mt@gmail.com>
2019-07-24 10:21:48 +0200

    First commit

---
+++ vue-app/app-vue/src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="about">
+        <h1> This is about view</h1>
+        <Tabla />
+        <el-button class="buttonP"
+                   type="primary"
+                   @click="goHome"
+        >
+            Go Home</el-button>
+    </div>
+</template>
+
+<script>
+import Tabla from "../components/Tabla.vue";
+export default {
+    name: "About",
+    props: {
+    msg: String
+    },
+    components: {
+      Tabla
+    },
+    methods: {
+        goHome() {
+            this.$router.push("/");
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+    .buttonP {
+        margin-top: 5%;
+    }
+</style>
---
+++ vue-app/app-vue/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h1>Hello this is home view</h1>
+    <Tabla />
+    <el-button class="buttonP" type="primary" @click="goAbout">
+      Go About</el-button
+    >
+  </div>
+</template>
+
+<script>
+import Tabla from "../components/Tabla.vue";
+export default {
+  name: "home",
+  components: {
+    Tabla
+  },
+  methods: {
+    goAbout() {
+      this.$router.push("/about");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.buttonP {
+  margin-top: 5%;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
